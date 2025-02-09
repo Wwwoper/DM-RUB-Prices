b9c25eb4c0304ba285212fdf141f31d4bd56378f
Ruslan Berenev <rulik8740@yandex.ru>
2025-02-09 21:39:56 +0300

    feat: Подготовка к добавлению поддержки сайта cocooncenter.de
    - Создан базовый плагин cocooncenter.js
    - Обновлен manifest.json для работы с новым доменом
    - Добавлен сайт в список поддерживаемых в README.md
    - Подготовлена базовая структура для конвертации цен

--- js/init-plugins.js
+++ js/init-plugins.js
@@ -4,4 +4,6 @@
     window.pluginManager.register(window.zaraPlugin);
     window.pluginManager.register(window.ikeaPlugin);
     window.pluginManager.register(window.parfumdreamsPlugin);
+    window.pluginManager.register(window.parfumsclubPlugin);
+    window.pluginManager.register(window.cocooncenterPlugin);
 })();
